a28ba086977625bdba7f8f20bb8b77a6e89b4ecf
Ryan <rmurph1800@gmail.com>
2021-12-21 12:13:58 -0500

    fixed paths

--- utils/generate-site.js
+++ utils/generate-site.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 
 const writeFile = fileContent => {
     return new Promise((resolve, reject) => {
-      fs.writeFile('./dist/index.html', fileContent, err => {
+      fs.writeFile('./index.html', fileContent, err => {
         // if there's an error, reject the Promise and send the error to the Promise's `.catch()` method
         if (err) {
           reject(err);
@@ -34,6 +34,6 @@ const writeFile = fileContent => {
         });
       });
     });
- };
+  };
 
 module.exports = { writeFile, copyFile };
